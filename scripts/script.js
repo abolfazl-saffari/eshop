a3679e96a8799ad67cc0eb5de94a1ad5aa169c61
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-01-04 14:05:18 +0330

    project started just now

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+const $ = document;
+let data = [];
+const mainElem = $.querySelector("main");
+
+async function getAllProducts() {
+  const response = await fetch("https://dummyjson.com/products");
+  const products = await response.json();
+
+  return products;
+}
+
+getAllProducts().then((products) => {
+  data = [...products.products];
+  productsGenerator(data);
+});
+
+function productsGenerator(products) {
+  console.log(products);
+  let allCards = "";
+  products.forEach((product) => {
+    const card = `
+    <div class="card" >
+        <img
+        src='${product.images[0]}'
+        alt='${product.title}'
+        class="card-img-top"
+        />
+        <div class="card-body">
+            <h6 class="card-title title m-0">${product.title}</h6>
+            <a  target=”_blank” class="btn btn-primary mt-4">Add to cart</a>
+        </div>
+    </div>`;
+    allCards += card;
+  });
+  mainElem.innerHTML = allCards;
+}

ce23754c29ac9a58f79221e7bb3ca9ffcf957d3b
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-01-05 23:15:37 +0330

    cart items, cart style added.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const $ = document;
 const mainElem = $.querySelector("main");
+const cartElem = $.querySelector(".cart-tooltip");
+let cart = [];
 let data = [];
 
 async function getAllProducts() {
@@ -9,13 +11,15 @@ async function getAllProducts() {
   return products;
 }
 
-getAllProducts().then((products) => {
-  data = [...products.products];
-  productsGenerator(data);
-});
+getAllProducts()
+  .then((products) => {
+    data = [...products.products];
+    productsGenerator(data);
+  })
+  .catch((error) => console.log(error));
 
 function productsGenerator(products) {
-  console.log(products);
+  cartGenerator(cart);
   let allCards = "";
   products.forEach((product) => {
     const card = `
@@ -27,10 +31,52 @@ function productsGenerator(products) {
         />
         <div class="card-body">
             <h6 class="card-title title m-0">${product.title}</h6>
-            <a  target=”_blank” class="btn btn-primary mt-4">Add to cart</a>
+
+            <button onclick="addToCart(${product.id})" class="btn btn-primary mt-4">Add to cart</button>
         </div>
     </div>`;
     allCards += card;
   });
   mainElem.innerHTML = allCards;
 }
+
+function cartGenerator(products) {
+  cartElem.innerHTML = "";
+  let allCards = "";
+  products.forEach((product) => {
+    const card = `
+    <div class="item d-flex justify-content-around align-items-center py-2 py-1 border-bottom">
+      <div class="d-flex">
+        <div class="d-flex justify-content-center align-self-center">
+          <img
+            class="rounded"
+            src='${product.images[0]}'
+            alt='${product.title}'
+          />
+        </div>
+        <div class="ms-3">
+          <p class="item-title m-0 text-dark">${product.title}</p>
+          <div>
+            <button class="rounded">-</button>
+            <p class="d-inline text-dark">1</p>
+            <button class="rounded">+</button>
+          </div>
+        </div>
+      </div>
+      <i class="bi bi-trash text-danger"></i>
+    </div>`;
+    allCards += card;
+  });
+  cartElem.innerHTML += allCards;
+}
+
+function addToCart(productID) {
+  let product = data.find((product) => product.id === productID);
+  cart.push(product);
+  cartGenerator(cart);
+}
+// <div class='mt-4'>
+// <button class='addBtn btn btn-primary'>-</button>
+// <p class='d-inline px-1'>1</p>
+// <button class='addBtn btn btn-primary'>+</button>
+// </div>
